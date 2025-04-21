cb6e02b26022382bd6a94708aa222ef15af26ad3
Elif Ergen <elifergen.1510@gmail.com>
2025-04-21 11:56:45 +0300

    🛒 A101 ürün verileri eklendi: scraper çıktısı ve CSV dosyası

---
+++ web_scraper/a101_veri_temp.py
@@ -0,0 +1,72 @@
+urunler = [
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22000838DEFAULT/22000838.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22001945DEFAULT/22001945DEFAULT_200.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22002270DEFAULT/22002270.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22000256DEFAULT/22000256DEFAULT_752.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22002001DEFAULT/22002001.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22002644DEFAULT/22002644.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22002455default/bingo-onaran-3l.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/23000604DEFAULT/23000604DEFAULT_868.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/23001486DEFAULT/1.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22002454default/bingo-renkli-3l.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22000279DEFAULT/22000279.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/23001487DEFAULT/1.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/23001282DEFAULT/23001282DEFAULT_106.jpg"
+  },
+  {
+    "ad": "Ürün adı alınamadı",
+    "fiyat": "Fiyat alınamadı",
+    "resim": "https://f-a101-l.mncdn.com/mnresize/480/480/livephotos/8/22001713DEFAULT/22001713.jpg"
+  }
+]
